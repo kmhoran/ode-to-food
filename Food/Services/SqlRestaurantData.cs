21b916c713b003a9f38c213c4e0e27bd7cfc24d8
Kevin Horan <Kevin.Horan@millennium-space.com>
2018-06-18 16:48:56 -0700

    include Entity Framework

---
+++ Food/Services/SqlRestaurantData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Food.Data;
+using Food.Models;
+
+namespace Food.Services
+{
+    public class SqlRestaurantData : IRestaurantData
+    {
+        private FoodDbContext _dbContext;
+
+        public SqlRestaurantData(FoodDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public int Add(Restaurant restaurant)
+        {
+            _dbContext.Restaurants.Add(restaurant);
+            _dbContext.SaveChanges();
+            return restaurant.RestaurantId;
+        }
+
+        public IEnumerable<Restaurant> Get()
+        {
+            return _dbContext.Restaurants.OrderBy(r => r.Name);
+        }
+
+        public Restaurant Get(int id)
+        {
+            return _dbContext.Restaurants.FirstOrDefault(r => r.RestaurantId == id);
+        }
+    }
+}
--- Food/Startup.cs
+++ Food/Startup.cs
@@ -1,8 +1,11 @@
-﻿using Food.Services;
+﻿using Food.Data;
+using Food.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -10,22 +13,38 @@ namespace Food
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        private IConfiguration _config;
+
+        //- Because we cannot inject our configuration details into the 
+        //- ConfigureServices method, we must initialize them in the constructor
+        public Startup(IConfiguration configuration)
+        {
+            _config = configuration;
+        }
+
+
+        //- This method gets called by the runtime. Use this method to add services to the container.
+        //- For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            // Singleton creates one service class to be used throughout the project
+            
+            //- Singleton creates one service class to be used throughout the project
             services.AddSingleton<IGreeter, Greeter>();
-            services.AddSingleton<IRestaurantData, InMemoryRestaurantData>();
+            // services.AddSingleton<IRestaurantData, InMemoryRestaurantData>();
 
-            // Scoped creates a new service class for each request
+
+            //- Scoped creates a new service class for each request
             // services.AddScoped<IRestaurantData, InMemoryRestaurantData>();
+            services.AddDbContext<FoodDbContext>(
+                options => options.UseSqlServer(_config.GetConnectionString("FoodDb")) // EF method to initialize service
+                ); 
+            services.AddScoped<IRestaurantData, SqlRestaurantData>();
 
-            // Required to use MVC middleware
+            //- Required to use MVC middleware
             services.AddMvc();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        //- This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, 
                               IHostingEnvironment env, 
                               IGreeter greeter, 
@@ -33,7 +52,7 @@ namespace Food
         {
             if (env.IsDevelopment())
             {
-                // Use verbose exceptions when in development
+                //- Use verbose exceptions when in development
                 app.UseDeveloperExceptionPage();
             }
 
@@ -42,13 +61,13 @@ namespace Food
                 Path="/welcome"
             });
 
-            // app.UseFileServer(); => combos UseDefeultFile & UseStaticFiles()
+            //- app.UseFileServer(); => combos UseDefeultFile & UseStaticFiles()
 
-            // If user directly accesses static file (e.g. /index.html), serve it, otherwise...
+            //- If user directly accesses static file (e.g. /index.html), serve it, otherwise...
             app.UseStaticFiles();
 
-            // Serve MVC routes
-            // Requires MVC service
+            //- Serve MVC routes
+            //- Requires MVC service
             app.UseMvc(ConfigureRoutes);
 
 
@@ -62,7 +81,7 @@ namespace Food
 
         private void ConfigureRoutes(IRouteBuilder routeBuilder)
         {
-            // /Home/Index/maybeSomeId
+            //- /Home/Index/maybeSomeId
             routeBuilder.MapRoute("Default", "{controller=Home}/{action=Index}/{id?}");
         }
     }
